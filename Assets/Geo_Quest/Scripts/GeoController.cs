a6e75a9cab160865d64fac38bc55e23071c97f0e
MehriddinP <pulotovmehriddin048@gmail.com>
2025-09-27 12:27:13 -0400

    C#
    c#

---
+++ Assets/Geo_Quest/Scripts/GeoController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Collections.LowLevel.Unsafe;
+using UnityEditor;
+using UnityEngine;
+
+public class jeocontroller : MonoBehaviour
+{
+
+
+
+    string varible1 = "hello";
+    int varible2 =  11375;
+    // Start is called before the first frame update
+    void Start()
+    {
+     Debug.Log("Hello World");
+     Debug.Log(varible1 + varible2);
+     
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
