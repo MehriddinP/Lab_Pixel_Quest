69da9dfe0ce87ad0cf1b64a57bee589d3b556cc9
MehriddinP <pulotovmehriddin048@gmail.com>
2025-11-08 12:17:09 -0500

    11
    11

--- Assets/Pixel_Quest/Scripts/PlayerMovement.cs
+++ Assets/Pixel_Quest/Scripts/PlayerMovement.cs
@@ -5,18 +5,24 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D _rigidbody2D;
+    private SpriteRenderer _spriterenderer;
     public int speed = 4;
 
     // Start is called before the first frame update
     void Start()
     {
-       _rigidbody2D = GetComponent<Rigidbody2D>(); 
+       _rigidbody2D = GetComponent<Rigidbody2D>();
+       _spriterenderer = transform.GetChild(0).GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
         float horizontal = Input.GetAxis("Horizontal");
+
+        if (horizontal > 0) { _spriterenderer.flipX = true; }
+        if(horizontal < 0) { _spriterenderer.flipX = false; }
+
         _rigidbody2D.velocity = new Vector2(horizontal *  speed, _rigidbody2D.velocity.y);
     }
 }
--- Assets/Pixel_Quest/Scripts/PlayerStats.cs
+++ Assets/Pixel_Quest/Scripts/PlayerStats.cs
@@ -1,26 +1,62 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Globalization;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class PlayerStats : MonoBehaviour
 {
+    public Transform respawPoint;
+    private int _health = 3;
+    private int _maxHealth = 3;
     public string nextLevel = "GeoLevel2";
+    private int coinCounter = 0;
   private void OnTriggerEnter2D(Collider2D collision)
   {
     switch (collision.tag)
     {
         case "Death":
             {
-                string thisLevel = SceneManager.GetActiveScene().name;
-                SceneManager.LoadScene(thisLevel);
-                break;
+              
+                    _health--;
+                    if (_health < 0)
+                    {
+                        string thisLevel = SceneManager.GetActiveScene().name;
+                        SceneManager.LoadScene(thisLevel);
+                    }
+                    else
+                    {
+                        transform.position = respawPoint.position;
+                    }
+                        break;
+
+
             }
+        case "Coin":
+                {
+                    coinCounter++;
+                    Destroy(collision.gameObject);
+                    break;
+                }
+
         case "Finish":
             {
+                    string nextLevel = collision.transform.GetComponent<LevelGoal>().nextLevel;
                 SceneManager.LoadScene(nextLevel);
                 break;
             }
+        case "Health":
+                {
+                    _health++;
+                    Destroy(collision.gameObject);
+                    if (_health < _maxHealth)
+
+                    {
+                        Destroy(collision.gameObject);
+                    }
+                    break;
+
+                }
     }
   }
 }
