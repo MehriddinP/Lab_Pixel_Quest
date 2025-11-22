1209d798839e7d51ff951df26b8efc16345eaa6a
MehriddinP <pulotovmehriddin048@gmail.com>
2025-11-22 10:52:09 -0500

    project 1
    project 1

---
+++ Assets/New Folder/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public float speed = 5f; // скорость движения
+
+    void Update()
+    {
+        // Получаем ввод
+        float x = Input.GetAxisRaw("Horizontal"); // влево/вправо
+        float y = Input.GetAxisRaw("Vertical");   // вверх/вниз
+
+        // Направление движения
+        Vector2 move = new Vector2(x, y);
+
+        // Двигаем героя
+        transform.position += (Vector3)move * speed * Time.deltaTime;
+    }
+
+}
